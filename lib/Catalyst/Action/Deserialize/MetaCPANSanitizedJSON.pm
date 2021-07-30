501cadcf82f105009a42e40a5522ef3999ce9819
Hugo (domibay-hugo) <hugo@domibay.org>
2021-07-30 13:08:19 +0100

    undo Catalyst::Action::Deserialize changes

--- lib/Catalyst/Action/Deserialize/MetaCPANSanitizedJSON.pm
+++ lib/Catalyst/Action/Deserialize/MetaCPANSanitizedJSON.pm
@@ -6,8 +6,6 @@ use Try::Tiny qw( catch try );
 use Cpanel::JSON::XS                 ();
 use MetaCPAN::Server::QuerySanitizer ();
 
-use Data::Dump qw(dump);
-
 extends 'Catalyst::Action::Deserialize::JSON';
 
 around execute => sub {
@@ -29,35 +27,6 @@ around execute => sub {
                         ->query );
             }
         }
-        else {  #JSON Decode failed
-            if ( ref $result ne '' ) {
-                if ( defined $result->{'message'} ) {
-                    my @arrdescription = ( $result->{'message'} =~ m/^(.*) at ([^\s]+) (line .*)$/mi );
-
-
-                    print "arr desc dmp:\n", dump @arrdescription ;
-                    print "\n";
-
-                    $c->detach( '/bad_request_json'
-                        , [ { 'exception_type' => ref($result), 'description' => $arrdescription[0]
-                            , 'file' => $arrdescription[1], 'lines' => $arrdescription[2] } ] );
-
-                    $c->error([ { 'exception_type' => ref($result), 'description' => $arrdescription[0]
-                            , 'file' => $arrdescription[1], 'lines' => $arrdescription[2] } ]);
-
-                    #Set Result to the Exception Message
-                    $result = $arrdescription[0];
-                }
-                else {  #The Result has no "message" Field
-                    $c->detach( '/bad_request_json'
-                        , [ { 'exception' => $result } ] );
-                }
-            }
-            else {  #The Result is not a Reference
-                $c->detach( '/bad_request_json'
-                    , [ { 'description' => $result } ] );
-            } #if ( ref $result ne '' )
-        } #if ( $result eq '1' )
 
         foreach my $attr (qw( query_parameters parameters )) {
 
@@ -92,16 +61,8 @@ around execute => sub {
         my $e = $_[0];
         if ( try { $e->isa('MetaCPAN::Server::QuerySanitizer::Error') } ) {
 
-            my @arrdescription = ($e->message =~ m/^(.*) at ([^\s]+) (line .*)$/mi);
-
-            # this will return a 400 (text) through Catalyst::Action::Deserialize
-            #$result = $e->message;
-            $result = $arrdescription[0];
-
-            $c->detach( '/bad_request_json'
-                , [ { 'exception_type' => ref($result), 'description' => $arrdescription[0]
-                    , 'file' => $arrdescription[1], 'lines' => $arrdescription[2] } ] );
-
+         # this will return a 400 (text) through Catalyst::Action::Deserialize
+            $result = $e->message;
 
             # this is our custom version (403) that returns json
             $c->detach( "/not_allowed", [ $e->message ] );
@@ -113,29 +74,6 @@ around execute => sub {
 
     return $result;
 };
-
-after 'execute' => sub {
-    my ( $self, $controller, $c ) = @_;
-
-    print "'" . (caller(1))[3] . "' : Signal to '" . (caller(0))[3] . "'\n";
-
-    print "State '", $c->state, "'\n";
-    print "Status Code [", $c->res->code, "]\n";
-    print "Content-Type: '", $c->res->content_type ,"'\n" ;
-    print "Response Body: '", $c->res->body ,"'\n" ;
-
-    if ( $c->has_errors ) {
-        print "execute finished with errors!\n";
-        print "arr err dmp:\n", dump $c->errors ;
-        print "\n";
-
-        #$c->clear_errors;
-    }
-    else {
-        print "execute finished - no errors.\n";
-    }
-};
-
 
 __PACKAGE__->meta->make_immutable;
 
